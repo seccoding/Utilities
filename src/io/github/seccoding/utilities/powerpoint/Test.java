920e89fad3247ccbfbacd94c6d7f6e0f4da6253e
mcjan <mcjan@DESKTOP-2BPOPPR>
2019-02-15 16:50:13 +0900

    패키지 변경

---
+++ src/io/github/seccoding/utilities/powerpoint/Test.java
@@ -0,0 +1,19 @@
+package io.github.seccoding.utilities.powerpoint;
+
+import java.io.File;
+
+import io.github.seccoding.utilities.powerpoint.Converter.ConvertResult;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Converter converter = new Converter();
+		ConvertResult result = converter.convert(new File("D:\\Java_신교재.pptx"), "11", "jpg");
+		
+		System.out.println(result.getFileName());
+		System.out.println(result.getOutputFolder());
+		System.out.println(result.getPageSize());
+		System.out.println(result.getOriginalFilePath());
+	}
+	
+}
