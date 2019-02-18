6e29a2c967e453f2354ee8993967bb86b8029fe0
Min Chang Jang <mcjang1116@gmail.com>
2019-02-18 16:10:11 +0900

    분리된 기능들 제거

--- src/io/github/seccoding/utilities/web/checker/ExtFilter.java
+++
@@ -1,9 +0,0 @@
-package io.github.seccoding.utilities.web.checker;
-
-public interface ExtFilter {
-
-	public static final int COMPARE_EXTENSION = 1;
-	public static final int APACHE_TIKA = 2;
-	public static final int JMIME_MAGIC = 3;
-	
-}
